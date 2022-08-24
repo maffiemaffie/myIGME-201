0f47460ed0de9a970e1e84827103546475344949
maffiemaffie <111717714+maffiemaffie@users.noreply.github.com>
2022-08-24 17:48:49 -0400

    :)

---
+++ Cohen_HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    // Author: Maffie Cohen
+    // Purpose: to practice syntax in C#
+    // Restrictions: 
+    static class Program
+    {
+        // Purpose: Write a few lines to the console
+        // Restrictions: 
+        static void Main(string[] args)
+        {
+            int num = 15;
+            string str = "3";
+            Console.WriteLine("Maffie Cohen");
+            Console.WriteLine("num + str = " + num + str);
+            Console.WriteLine("num + num = " + (num + num)); // parentheses evaluate math before concatenating to string
+
+            int counter = 0;
+            while (++counter < 10)
+            {
+                Console.Write(counter + "");
+            }
+        }
+    }
+}
