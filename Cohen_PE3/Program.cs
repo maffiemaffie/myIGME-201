250ee1e0a627c049b89e509565a377e884a75da4
Mafffie Cohen <mrc6113@rit.edu>
2022-09-02 01:36:21 -0400

    PE3 - Variables and Expressions

---
+++ Cohen_PE3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Cohen_PE3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                Console.WriteLine("ERROR: Invalid parameters, expected four integers, receieved " +
+                    args.ToString());
+            } else
+            {
+                int product = 1; // multiplicative identity
+                bool isError = false;
+                for (int i = 0; i < 4; i++)
+                {
+                    try
+                    {
+                        product *= Convert.ToInt32(args[i]);
+                    } catch
+                    {
+                        Console.WriteLine("ERROR: Invalid parameter, " + args[i] + " is not a valid integer.");
+                        isError = true;
+                        break;
+                    }   
+                }
+                if (!isError)
+                {
+                    Console.WriteLine(product);
+                }
+            }
+        }
+    }
+}
