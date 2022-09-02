25bb459d1d8b2e466483df5d22fcd2a8773ab232
Mafffie Cohen <mrc6113@rit.edu>
2022-09-02 02:19:13 -0400

    PE4 Flow Control

---
+++ Cohen_PE4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cohen_PE4
+{
+    // Author: Maffie Cohen
+    // Purpose: Displays two numbers if only one is greater than 10
+    // Restrictions:
+    static class Program
+    {
+        // Purpose: Asks for numbers and either displays them or asks for new ones
+        // Restrictions:
+        static void Main(string[] args)
+        {
+            double num1;
+            double num2;
+            do
+            {
+                Console.WriteLine("Hello please give me a couple numbers");
+                num1 = Convert.ToDouble(Console.ReadLine());
+                num2 = Convert.ToDouble(Console.ReadLine());
+            } while (((num1 > 10) && (num2 > 10)) || ((num1 < 10) && (num2 < 10)));
+            Console.WriteLine(num1 + " " + num2);
+
+        }
+    }
+}
