d593bef4f81f92a1cec017512818fe8f10ad9102
Mafffie Cohen <mrc6113@rit.edu>
2022-09-30 19:33:39 -0400

    Traffic and Vehicles

---
+++ Traffic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vehicles;
+
+namespace Traffic
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPassengerCarrier suv = new SUV();
+
+            AddPassenger(ref suv);
+        }
+
+        static void AddPassenger(ref IPassengerCarrier vehicleObject)
+        {
+            vehicleObject.LoadPassenger();
+            Console.WriteLine(vehicleObject.ToString());
+        }
+    }
+}
