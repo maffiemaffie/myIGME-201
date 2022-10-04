70d11f57fc21ac9065a242ecb852f241d90ab46c
Mafffie Cohen <mrc6113@rit.edu>
2022-10-03 21:15:44 -0400

    PE12

---
+++ Cohen_PE12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cohen_PE12
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass testClass = new MyDerivedClass();
+            Console.WriteLine(testClass.GetString());
+        }
+
+        public class MyClass
+        {
+            private string MyString;
+            public virtual string GetString()
+            {
+                return MyString;
+            }
+        }
+
+        public class MyDerivedClass : MyClass
+        {
+            public virtual string GetString()
+            {
+                return base.GetString() + " (output from the derived class)";
+            }
+        }
+    }
+}
