92d103cb556c96d8a5e124d6d65c3869b7163d0b
Mafffie Cohen <mrc6113@rit.edu>
2022-10-22 23:23:51 -0400

    12

---
+++ Cohen_UT2_12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cohen_UT2_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, DateTime> friendBirthdays = new SortedList<string, DateTime>();
+            foreach (DateTime value in friendBirthdays.Values)
+            {
+                Console.WriteLine(value.ToString("MM/dd/yyyy"));
+            }
+        }
+    }
+}
