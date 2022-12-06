cd1f9196ef55c8d6126399c07bf09c8a6b633372
Mafffie Cohen <mrc6113@rit.edu>
2022-12-06 08:28:28 -0500

    Dyscord

---
+++ Dyscord/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Dyscord
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.listenerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(121, 81);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Listener";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(121, 43);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(100, 20);
+            this.portTextBox.TabIndex = 1;
+            // 
+            // listenerLabel
+            // 
+            this.listenerLabel.AutoSize = true;
+            this.listenerLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.listenerLabel.Location = new System.Drawing.Point(46, 50);
+            this.listenerLabel.Name = "listenerLabel";
+            this.listenerLabel.Size = new System.Drawing.Size(69, 13);
+            this.listenerLabel.TabIndex = 2;
+            this.listenerLabel.Text = "Listener Port:";
+            // 
+            // SettingsForm
+            // 
+            this.AcceptButton = this.startButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 150);
+            this.ControlBox = false;
+            this.Controls.Add(this.listenerLabel);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.startButton);
+            this.Name = "SettingsForm";
+            this.Text = "Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label listenerLabel;
+    }
+}
+
