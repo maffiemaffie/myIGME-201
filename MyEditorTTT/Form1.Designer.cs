e31304ca8dace7c6a8ff657de52c776d493837d8
Mafffie Cohen <mrc6113@rit.edu>
2022-12-05 21:01:56 -0500

    MyEditorTTT

--- MyEditorTTT/Form1.Designer.cs
+++ MyEditorTTT/Form1.Designer.cs
@@ -41,7 +41,6 @@
             this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.toolStrip = new System.Windows.Forms.ToolStrip();
             this.boldToolStripButton = new System.Windows.Forms.ToolStripButton();
             this.italicsToolStripButton = new System.Windows.Forms.ToolStripButton();
@@ -59,13 +58,20 @@
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.colorDialog = new System.Windows.Forms.ColorDialog();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.testToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.toolStripContainer = new System.Windows.Forms.ToolStripContainer();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.countdownLabel = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
             this.toolStrip.SuspendLayout();
             this.contextMenuStrip.SuspendLayout();
+            this.toolStripContainer.BottomToolStripPanel.SuspendLayout();
+            this.toolStripContainer.ContentPanel.SuspendLayout();
+            this.toolStripContainer.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -158,37 +164,19 @@
             this.pasteToolStripMenuItem.Text = "&Paste";
             this.pasteToolStripMenuItem.ToolTipText = "Paste";
             // 
-            // splitContainer1
-            // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainer1.IsSplitterFixed = true;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer1.Panel1
-            // 
-            this.splitContainer1.Panel1.Controls.Add(this.toolStrip);
-            // 
-            // splitContainer1.Panel2
-            // 
-            this.splitContainer1.Panel2.Controls.Add(this.richTextBox);
-            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
-            this.splitContainer1.SplitterDistance = 25;
-            this.splitContainer1.TabIndex = 1;
-            // 
             // toolStrip
             // 
+            this.toolStrip.Dock = System.Windows.Forms.DockStyle.None;
             this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.boldToolStripButton,
             this.italicsToolStripButton,
             this.underlineToolStripButton,
             this.colorToolStripButton,
-            this.fontToolStripButton});
-            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.fontToolStripButton,
+            this.testToolStripButton});
+            this.toolStrip.Location = new System.Drawing.Point(10, 0);
             this.toolStrip.Name = "toolStrip";
-            this.toolStrip.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip.Size = new System.Drawing.Size(156, 25);
             this.toolStrip.TabIndex = 0;
             this.toolStrip.Text = "toolStrip1";
             // 
@@ -244,14 +232,14 @@
             // mSSansSerifToolStripMenuItem
             // 
             this.mSSansSerifToolStripMenuItem.Name = "mSSansSerifToolStripMenuItem";
-            this.mSSansSerifToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.mSSansSerifToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
             this.mSSansSerifToolStripMenuItem.Text = "MS Sans Serif";
             this.mSSansSerifToolStripMenuItem.ToolTipText = "MS Sans Serif";
             // 
             // timesNewRomanToolStripMenuItem
             // 
             this.timesNewRomanToolStripMenuItem.Name = "timesNewRomanToolStripMenuItem";
-            this.timesNewRomanToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.timesNewRomanToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
             this.timesNewRomanToolStripMenuItem.Text = "Times New Roman";
             this.timesNewRomanToolStripMenuItem.ToolTipText = "Times New Roman";
             // 
@@ -261,7 +249,7 @@
             this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richTextBox.Location = new System.Drawing.Point(0, 0);
             this.richTextBox.Name = "richTextBox";
-            this.richTextBox.Size = new System.Drawing.Size(800, 397);
+            this.richTextBox.Size = new System.Drawing.Size(800, 379);
             this.richTextBox.TabIndex = 0;
             this.richTextBox.Text = "";
             // 
@@ -305,26 +293,91 @@
             this.saveFileDialog.DefaultExt = "rtf";
             this.saveFileDialog.Filter = "Rich Text Files|*.rtf|Plain Text Files|*.txt|All Files|*.*";
             // 
+            // testToolStripButton
+            // 
+            this.testToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.testToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("testToolStripButton.Image")));
+            this.testToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.testToolStripButton.Name = "testToolStripButton";
+            this.testToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.testToolStripButton.Text = "toolStripButton1";
+            // 
+            // toolStripContainer
+            // 
+            // 
+            // toolStripContainer.BottomToolStripPanel
+            // 
+            this.toolStripContainer.BottomToolStripPanel.Controls.Add(this.statusStrip1);
+            // 
+            // toolStripContainer.ContentPanel
+            // 
+            this.toolStripContainer.ContentPanel.Controls.Add(this.countdownLabel);
+            this.toolStripContainer.ContentPanel.Controls.Add(this.richTextBox);
+            this.toolStripContainer.ContentPanel.Size = new System.Drawing.Size(800, 379);
+            this.toolStripContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer.Location = new System.Drawing.Point(0, 24);
+            this.toolStripContainer.Name = "toolStripContainer";
+            this.toolStripContainer.Size = new System.Drawing.Size(800, 426);
+            this.toolStripContainer.TabIndex = 1;
+            this.toolStripContainer.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer.TopToolStripPanel
+            // 
+            this.toolStripContainer.TopToolStripPanel.Controls.Add(this.toolStrip);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 0);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Maximum = 60;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(700, 16);
+            this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.countdownLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.Color.Red;
+            this.countdownLabel.Location = new System.Drawing.Point(0, 0);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(800, 379);
+            this.countdownLabel.TabIndex = 2;
+            this.countdownLabel.Text = "3";
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStripContainer);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.PerformLayout();
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
             this.toolStrip.ResumeLayout(false);
             this.toolStrip.PerformLayout();
             this.contextMenuStrip.ResumeLayout(false);
+            this.toolStripContainer.BottomToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer.BottomToolStripPanel.PerformLayout();
+            this.toolStripContainer.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer.ResumeLayout(false);
+            this.toolStripContainer.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -343,7 +396,6 @@
         private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
-        private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.RichTextBox richTextBox;
         private System.Windows.Forms.ToolStrip toolStrip;
         private System.Windows.Forms.ToolStripButton boldToolStripButton;
@@ -361,6 +413,12 @@
         private System.Windows.Forms.ToolStripMenuItem boldToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem italicsToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem underlineToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton testToolStripButton;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Label countdownLabel;
     }
 }
 
---
+++ SearchLib/SearchLib/Class1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchLib
+{
+    public class SearchResult : IComparable
+    {
+        public Course course;
+        public int relevance;
+
+        public string Name { get { return course.Name; } }
+
+        public SearchResult(Course course, int relevance)
+        {
+            this.course = course;
+            this.relevance = relevance; 
+        }
+
+        private class SortRelevanceHelper : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                SearchResult s1 = (SearchResult)a;
+                SearchResult s2 = (SearchResult)b;
+                return s1.relevance - s2.relevance;
+            }
+        }
+
+        private class SortAlphabeticalDescendingHelper : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                SearchResult s1 = (SearchResult)a;
+                SearchResult s2 = (SearchResult)b;
+                return s1.Name.CompareTo(s2.Name);
+            }
+        }
+
+        private class SortAlphabeticalAscendingHelper : IComparer
+        {
+            int IComparer.Compare(object a, object b)
+            {
+                SearchResult s1 = (SearchResult)a;
+                SearchResult s2 = (SearchResult)b;
+                return s2.Name.CompareTo(s1.Name);
+            }
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            SearchResult s = (SearchResult)obj;
+            return SortAlphabeticalDescending().Compare(this, s);
+        }
+
+        public static IComparer SortRelevance()
+        {
+            return (IComparer) new SortRelevanceHelper();
+        }
+
+        public static IComparer SortAlphabeticalDescending()
+        {
+            return (IComparer)new SortAlphabeticalDescendingHelper();
+        }
+
+        public static IComparer SortAlphabeticalAscending()
+        {
+            return (IComparer)new SortAlphabeticalAscendingHelper();
+        }
+
+    }
+
+    public interface IQuerySimilar
+    {
+        int GetDistance(Course course);
+    }
+
+    public abstract class QueryCondition { }
+
+    public abstract class QueryCondition<T> : QueryCondition
+    {
+        public T Query;
+
+        public QueryCondition(T query)
+        {
+            Query = query;
+        }
+
+        public abstract bool IsSatisfied(Course course);
+    }
+
+    public class QueryNumber : QueryCondition<int>, IQuerySimilar
+    {
+        public QueryNumber(int query) : base(query) { }
+
+        public override bool IsSatisfied(Course course)
+        {
+
+        }
+
+        public int GetDistance(Course course)
+        {
+
+        }
+    }
+
+    public class QueryCode : QueryCondition<int>, IQuerySimilar
+    {
+        public QueryCode(int query) : base(query) { }
+
+        public override bool IsSatisfied(Course course)
+        {
+
+        }
+
+        public int GetDistance(Course course)
+        {
+
+        }
+    }
+
+    public class QueryKeyword : QueryCondition<string>, IQuerySimilar
+    {
+        public QueryKeyword(string query) : base(query) { }
+
+        public override bool IsSatisfied(Course course)
+        {
+
+        }
+
+        public int GetDistance(Course course)
+        {
+
+        }
+    }
+
+    public class QuerySubject : QueryCondition<string>, IQuerySimilar
+    {
+        public QuerySubject(string query) : base(query) { }
+
+        public override bool IsSatisfied(Course course)
+        {
+
+        }
+
+        public int GetDistance(Course course)
+        {
+
+        }
+    }
+
+    public class QueryTimes : QueryCondition<(DateTime, DateTime)>
+    {
+        public QueryTimes((DateTime, DateTime) query) : base(query) { }
+
+        public override bool IsSatisfied(Course course)
+        {
+
+        }
+    }
+
+    public class QueryAvailability : QueryCondition<Availability>
+    {
+        public QueryAvailability(Availability query) : base(query) { }
+
+        public override bool IsSatisfied(Course course)
+        {
+
+        }
+    }
+
+    public class QueryPerspective : QueryCondition<string>
+    {
+        public QueryPerspective(string query) : base(query) { }
+
+        public override bool IsSatisfied(Course course)
+        {
+
+        }
+    }
+
+    public class QueryMajor : QueryCondition<string>
+    {
+        public QueryMajor(string query) : base(query) { }
+
+        public override bool IsSatisfied(Course course)
+        {
+
+        }
+    }
+
+    public class QueryDays : QueryCondition<Days>
+    {
+        public QueryDays(Days query) : base(query) { }
+
+        public override bool IsSatisfied(Course course)
+        {
+
+        }
+    }
+
+    public class SearchManager
+    {
+        private SortType sorting;
+        public SortType Sorting { get { return sorting; } set { sorting = value; } }
+
+        public List<SearchResult> Search(List<QueryCondition> query)
+        {
+            
+        }
+    }
+
+    public enum SortType
+    {
+        AlphabetAscending,
+        AlphabetDescending,
+        Relevance
+    }
+
+    [Flags]
+    public enum Availability
+    {
+        Open,
+        Waitlist,
+        Closed
+    }
+
+    [Flags]
+    public enum Days
+    {
+        Monday = 0b_0000_0001,
+        Tuesday = 0b_0000_0010,
+        Wednesday = 0b_0000_0100,
+        Thursday = 0b_0000_1000,
+        Friday = 0b_0001_0000,
+        Saturday = 0b_0010_0000,
+        Sunday = 0b_0100_0000
+    }
+}
