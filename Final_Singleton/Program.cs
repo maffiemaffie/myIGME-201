4f03f5b48cb4600f68bf8a8c9c7b3cd2cc412560
Mafffie Cohen <mrc6113@rit.edu>
2022-12-13 21:47:21 -0500

    maffie

--- Final_Singleton/Program.cs
+++ Final_Singleton/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 using Newtonsoft.Json;
-using System.IO;
 
 namespace Singleton
 {
